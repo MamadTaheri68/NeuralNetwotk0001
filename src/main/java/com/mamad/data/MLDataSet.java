d8479dbcaf16834dc5f1f21b770ca9f0524e6d5a
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-10-24 22:07:05 +0330

    implement data functions

---
+++ src/main/java/com/mamad/data/MLDataSet.java
@@ -0,0 +1,27 @@
+package com.mamad.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class MLDataSet {
+
+    private double[][] inputs;
+    private double[][] targets;
+    private List<MLData> data;
+
+    public MLDataSet(double[][] inputs, double[][] targets) {
+        this.data = new ArrayList<>();
+        this.inputs = inputs;
+        this.targets = targets;
+        for (int i = 0; i < this.inputs.length; i++) {
+            this.data.add(new MLData(inputs[i],targets[i]));
+        }
+    }
+
+
+}
